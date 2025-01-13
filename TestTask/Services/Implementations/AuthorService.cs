6799412bd678d11c6fefda78698d7f59b853d2fa
Mikhail Kapytok <mishakopytok@gmail.com>
2025-01-13 19:06:30 +0300

    created services

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService(ApplicationDbContext applicationDbContext) : IAuthorService
+    {
+        private readonly ApplicationDbContext _context = applicationDbContext;
+        public Task<Author> GetAuthor()
+        {
+            var dbResp = _context.Authors
+                .AsNoTracking()
+                .Select(author => new
+                {
+                    Author = author,
+                    LongestBookTitleLength = author.Books.Max(book => book.Title.Length)
+                })
+                .OrderByDescending(a => a.LongestBookTitleLength)
+                .ThenBy(a => a.Author.Id)
+                .FirstOrDefault()?.Author;
+
+            return Task.FromResult(dbResp);
+        }
+
+        public Task<List<Author>> GetAuthors()
+        {
+            var dbResp = _context.Authors
+                .AsNoTracking()
+                .Where(author => author.Books
+                    .Count(book => book.PublishDate >= new DateTime(2016, 1, 1)) % 2 == 0)
+                .ToList();
+            return Task.FromResult(dbResp);
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService(ApplicationDbContext applicationDbContext) : IBookService
+    {
+        private readonly ApplicationDbContext _context = applicationDbContext;
+        public Task<Book> GetBook()
+        {
+            var dbResp = _context.Books
+                .AsNoTracking()
+                .OrderByDescending(book => book.Price * book.QuantityPublished)
+                .FirstOrDefault();
+            return Task.FromResult(dbResp);
+        }
+
+        public Task<List<Book>> GetBooks()
+        {
+            var carolusRexReleaseDate = new DateTime(2012, 5, 25);
+            var dbResp = _context.Books
+                .AsNoTracking()
+                .Where(book => book.PublishDate >  carolusRexReleaseDate 
+                    && book.Title.Contains("Red"))
+                .ToList();
+            return Task.FromResult(dbResp);
+        }
+    }
+}
